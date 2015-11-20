53cffdae8a11445a5e610fbcdd11f89e1230c9f1
Abhijit Das <abhijit2295@hotmail.com>
2015-11-20 11:29:56 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,3 +10,31 @@ $('section').on('click', function(){
   $(this).addClass('active');
   $('.contain').removeClass('active');
 })
+// background video
+
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
