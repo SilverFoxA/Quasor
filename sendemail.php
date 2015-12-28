cef31e88c56c4fce6fcbbaa77b42c653adf917b4
silverFoxA <abhijit2294@hotmail.com>
2015-12-28 12:31:33 +0530

    update

--- sendemail.php
+++ sendemail.php
@@ -1,9 +1,9 @@
 <?php
 $name       = @trim(stripslashes($_POST['name'])); 
-$from       = 'mayank@quasor.in'
+$from       = @trim(stripslashes($_POST['email'])); 
 $subject    = 'Enquiry'; 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'abhijit2294@gmail.com';
+$to   		= 'mayank@quasor.in';
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
@@ -13,23 +13,6 @@ $headers[] = "Reply-To: <{$from}>";
 $headers[] = "Subject: {$subject}";
 $headers[] = "X-Mailer: PHP/".phpversion();
 
-server {
-    listen       80;
-    server_name  localhost;
-
-    location / {
-        root   html;
-        index  index.html index.htm;
-    }
-
-    error_page  404     /404.html;
-    error_page  403     /403.html;
-
-    # To allow POST on static pages
-    error_page  405     =200 $uri;
-
-    # ...
-}
 
 mail($to, $subject, $message, $headers);
 
